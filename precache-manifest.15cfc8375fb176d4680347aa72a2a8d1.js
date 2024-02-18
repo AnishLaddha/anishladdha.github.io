f20c8fe146ba9af00792768d1fffd45c378e0357
AnishLaddha <anishladdha03@gmail.com>
2024-02-18 04:23:31 -0500

    Updates

---
+++ precache-manifest.15cfc8375fb176d4680347aa72a2a8d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c60a86bc1502acfad6b658536962fdb",
+    "url": "/anishladdha.github.io/index.html"
+  },
+  {
+    "revision": "a76eed0c99297ed8a7eb",
+    "url": "/anishladdha.github.io/static/css/main.dea78873.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/anishladdha.github.io/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/anishladdha.github.io/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "a76eed0c99297ed8a7eb",
+    "url": "/anishladdha.github.io/static/js/main.78fdba79.chunk.js"
+  },
+  {
+    "revision": "70d03f76a8cd5e8b71af",
+    "url": "/anishladdha.github.io/static/js/runtime~main.942fc45c.js"
+  },
+  {
+    "revision": "8ae571bfe4139f51f21fa065700675c7",
+    "url": "/anishladdha.github.io/static/media/comp.8ae571bf.gif"
+  }
+]);
