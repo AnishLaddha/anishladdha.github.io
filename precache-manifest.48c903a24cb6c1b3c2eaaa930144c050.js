021574f36fbba3be4d8cacdadfe93c86f4574b30
AnishLaddha <anishladdha03@gmail.com>
2024-02-18 04:31:05 -0500

    Updates

---
+++ precache-manifest.48c903a24cb6c1b3c2eaaa930144c050.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f8c1fb2fae8bded00f0cad5cf60b07f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a76eed0c99297ed8a7eb",
+    "url": "/static/css/main.dea78873.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "a76eed0c99297ed8a7eb",
+    "url": "/static/js/main.78fdba79.chunk.js"
+  },
+  {
+    "revision": "e21c8a56d64ede50141d",
+    "url": "/static/js/runtime~main.891e0007.js"
+  },
+  {
+    "revision": "8ae571bfe4139f51f21fa065700675c7",
+    "url": "/static/media/comp.8ae571bf.gif"
+  }
+]);
