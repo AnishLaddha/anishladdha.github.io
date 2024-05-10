67056d182467f77cc2ce60706c51889496af94c9
AnishLaddha <anishladdha03@gmail.com>
2024-05-10 01:10:55 -0700

    updates

--- src/content/home.js
+++ src/content/home.js
@@ -6,6 +6,7 @@ const HomeContent = () => {
     <div>
       <img src={gifImage} alt="Example GIF" />
       <h1>Hello, I'm Anish Laddha</h1>
+      <h3>Above your tomb, the stars will belong to us.</h3>
       <h3>(Website under construction)</h3>
     </div>
   );
--- src/content/work.js
+++ src/content/work.js
@@ -11,80 +11,30 @@ const WorkContent = () => {
     
     
     <div className='we'>
-      <h2>Visa</h2>
-      <div className='we-title'>
-        <h3>Software Engineer Intern</h3>
-        <h3 styles={{paddingRight: "10%", textAlign:"right"}}> Summer 2023</h3>
-      </div>
-      <div className='we-list'>
-        <ul>
-          <li>Lorem ipsum dore</li>
-          <li>Lorem ipsum dore</li>
-          <li>Lorem ipsum dore</li>
-        </ul>
-      </div>
+      <h2>TBA ;) - Summer 2024</h2>
+      <h3>Software Engineer Intern</h3>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque viverra a sapien aliquet finibus. Pellentesque dapibus ex id ligula mollis gravida. Donec sed mauris in tortor laoreet maximus. Sed posuere magna sapien, sed faucibus ante blandit eget. Vestibulum a eros et ipsum vestibulum mollis et sit amet eros. Ut quis hendrerit ipsum, ut sagittis turpis. Aenean eget eleifend nibh, posuere elementum dui. Nulla blandit sodales lacus ut pulvinar.
+      </p>
     </div>
 
     <div className='we'>
-      <h2>Visa</h2>
-      <div className='we-title'>
-        <h3>Software Engineer Intern</h3>
-        <h3 styles={{paddingRight: "10%", textAlign:"right"}}> Summer 2022</h3>
-      </div>
-      <div className='we-list'>
-        <ul>
-          <li>Lorem ipsum dore</li>
-          <li>Lorem ipsum dore</li>
-          <li>Lorem ipsum dore</li>
-        </ul>
-      </div>
+      <h2>Visa - Summer 2023</h2>
+      <h3>Software Engineer Intern</h3>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque viverra a sapien aliquet finibus. Pellentesque dapibus ex id ligula mollis gravida. Donec sed mauris in tortor laoreet maximus. Sed posuere magna sapien, sed faucibus ante blandit eget. Vestibulum a eros et ipsum vestibulum mollis et sit amet eros. Ut quis hendrerit ipsum, ut sagittis turpis. Aenean eget eleifend nibh, posuere elementum dui. Nulla blandit sodales lacus ut pulvinar.
+      </p>
     </div>
 
     <div className='we'>
-      <h2>Visa</h2>
-      <div className='we-title'>
-        <h3>Software Engineer Intern</h3>
-        <h3 styles={{paddingRight: "10%", textAlign:"right"}}> Summer 2022</h3>
-      </div>
-      <div className='we-list'>
-        <ul>
-          <li>Lorem ipsum dore</li>
-          <li>Lorem ipsum dore</li>
-          <li>Lorem ipsum dore</li>
-        </ul>
-      </div>
+      <h2>Visa - Summer 2022</h2>
+      <h3>Software Engineer Intern</h3>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque viverra a sapien aliquet finibus. Pellentesque dapibus ex id ligula mollis gravida. Donec sed mauris in tortor laoreet maximus. Sed posuere magna sapien, sed faucibus ante blandit eget. Vestibulum a eros et ipsum vestibulum mollis et sit amet eros. Ut quis hendrerit ipsum, ut sagittis turpis. Aenean eget eleifend nibh, posuere elementum dui. Nulla blandit sodales lacus ut pulvinar.
+      </p>
     </div>
+
     
-    <div className='we'>
-      <h2>Visa</h2>
-      <div className='we-title'>
-        <h3>Software Engineer Intern</h3>
-        <h3 styles={{paddingRight: "10%", textAlign:"right"}}> Summer 2022</h3>
-      </div>
-      <div className='we-list'>
-        <ul>
-          <li>Lorem ipsum dore</li>
-          <li>Lorem ipsum dore</li>
-          <li>Lorem ipsum dore</li>
-        </ul>
-      </div>
-    </div>
-
-    <div className='we'>
-      <h2>Visa</h2>
-      <div className='we-title'>
-        <h3>Software Engineer Intern</h3>
-        <h3 styles={{paddingRight: "10%", textAlign:"right"}}> Summer 2022</h3>
-      </div>
-      <div className='we-list'>
-        <ul>
-          <li>Lorem ipsum dore</li>
-          <li>Lorem ipsum dore</li>
-          <li>Lorem ipsum dore</li>
-        </ul>
-      </div>
-    </div>
-
   </div>
   );
 };
