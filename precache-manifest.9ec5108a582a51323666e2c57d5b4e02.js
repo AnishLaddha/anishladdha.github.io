fbe1cb108f0d77e61406690a69a6d68f4b4094dc
AnishLaddha <anishladdha03@gmail.com>
2024-05-10 00:00:51 -0700

    Updates

---
+++ precache-manifest.9ec5108a582a51323666e2c57d5b4e02.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c4a746b6f1b4c6b1c5c5d5db24aa8f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dea01217dfdc333c5fcc",
+    "url": "/static/css/main.dea78873.chunk.css"
+  },
+  {
+    "revision": "bd0d5e29114cbea3c48c",
+    "url": "/static/js/2.28824cbc.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.28824cbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "016bdaa76e5af7b40a38",
+    "url": "/static/js/3.3f736f49.chunk.js"
+  },
+  {
+    "revision": "dea01217dfdc333c5fcc",
+    "url": "/static/js/main.c3cf8c4b.chunk.js"
+  },
+  {
+    "revision": "917c8bebf2474be7218c",
+    "url": "/static/js/runtime-main.e5d8d60d.js"
+  },
+  {
+    "revision": "8ae571bfe4139f51f21fa065700675c7",
+    "url": "/static/media/comp.8ae571bf.gif"
+  }
+]);
