9645f004850d9713feded8e3e45cb72577cb1f51
AnishLaddha <anishladdha03@gmail.com>
2024-05-11 17:08:46 -0700

    done

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -15,7 +15,7 @@ const Sidebar = ({ onSelect }) => {
       <div className={`sb3 ${selected === 'A' ? 'selected' : ''}`} onClick={() => handleSelect('A')}>About</div>
       <div className={`sb3 ${selected === 'B' ? 'selected' : ''}`} onClick={() => handleSelect('B')}>Work</div>
       <div className={`sb3 ${selected === 'C' ? 'selected' : ''}`} onClick={() => handleSelect('C')}>Projects</div>
-      <div className={`sb3 ${selected === 'D' ? 'selected' : ''}`} onClick={() => handleSelect('D')}>Thoughts</div>
+      {/* <div className={`sb3 ${selected === 'D' ? 'selected' : ''}`} onClick={() => handleSelect('D')}>Thoughts</div> */}
       <div className={`sb3 ${selected === 'E' ? 'selected' : ''}`} onClick={() => handleSelect('E')}>Contact</div>
     </div>
   );
--- src/content/work.js
+++ src/content/work.js
@@ -5,7 +5,7 @@ const WorkContent = () => {
   return (
     <div className='work-content'>
     <a href="https://linkedin.com/in/AnishLaddha" target="_blank" rel="noopener noreferrer"><h2><i class="fa fa-file-text" aria-hidden="true"></i>&nbsp;Resume</h2></a>
-    <h1>Internships:</h1>  
+    <h1 class="w-head">Internships</h1>  
     <div className='we'>
       <h2>Coinbase - Summer 2024</h2>
       <h3>Software Engineer Intern</h3>
@@ -18,7 +18,7 @@ const WorkContent = () => {
       <h2>Visa - Summer 2023</h2>
       <h3>Software Engineer Intern</h3>
       <p class="work-desc">
-        During the summer of '23 I worked for the Artificial Intelligence Platform team out of Foster City, writing a program to optimize compute resources for Apache Spark jobs (mostly data processing and engineering). Working in python, I used <a href="https://github.com/qubole/sparklens" target="_blank" rel="noopener noreferrer" class="blue-link">Sparklens <i class="fa fa-external-link" aria-hidden="true"></i></a> and past data to profile a variety of jobs, detected bottlenecks based on resources, and then assigned an amount of vcores/vram to eliminate those bottlenecks.
+        During the summer of '23 I worked for the Artificial Intelligence Platform team out of Foster City, writing a program to optimize compute resources for Apache Spark jobs (mostly data processing and engineering). Working in python, I used <a href="https://github.com/qubole/sparklens" target="_blank" rel="noopener noreferrer" class="blue-link">Sparklens <i class="fa fa-external-link" aria-hidden="true"></i></a> and past data to profile a variety of jobs, detected bottlenecks based on resource allocation, and then used gradient descent to assign an optimal amount of vcores/vram to eliminate those bottlenecks.
         <br></br>
         <br></br>
         I created a <a href="https://grafana.com/" target="_blank" rel="noopener noreferrer" class="blue-link">Grafana <i class="fa fa-external-link" aria-hidden="true"></i></a> dashboard, powered by a Flask + SQL backend, to visualize all the data that this process generated. I was able to generate some pretty cool/insightful graphs. In fact, you could see some bottlenecks pop up, get detected, and get squashed in real time.
